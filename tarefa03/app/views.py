fd6599e8688dffee742bd9d66761d07d5665db25
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-06-13 18:00:00 -0300

    tarefa03

---
+++ tarefa03/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def index(request):
+    return render (request, 'index.html')
+
+def usuarios(request):
+    user_list = [
+        {"nome": "Computador Ferreira Andrade", "matricula": "1234321", "idade": 12, "cidade": "Acari"},
+        {"nome": "Microfone Oliveira da Silva", "matricula": "5678765", "idade": 19, "cidade": "Peruibe"},
+        {"nome": "Impressora Eliany Costas", "matricula": "1234321", "idade": 20, "cidade": "San Francisco"},
+        {"nome": "Maria Windows do Santos", "matricula": "1234321", "idade": 15, "cidade": "Nova Iorque"},
+        {"nome": "Linux Nascimento do Santos", "matricula": "1234321", "idade": 23, "cidade": "Nova Iorque"}
+    ]
+
+    context = {
+        "usuarios": user_list,
+    }
+    
+    return render(request, 'usuarios.html', context)
