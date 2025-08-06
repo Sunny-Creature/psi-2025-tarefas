2f75d482cf407aeeaf53172c5c911bb9aa7b30b4
Sunny-Creature <soniaisissoninha119@gmail.com>
2025-08-06 12:05:48 -0300

    finalizacao tarefa04

--- tarefa04/tarefas/models.py
+++ tarefa04/tarefas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    nome = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    prazo = models.DateField(auto_now='')
+
--- tarefa04/tarefas/views.py
+++ tarefa04/tarefas/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from datetime import date
+from .models import Tarefa
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context = {
+        'tarefas': Tarefa.objects.all(),
+    }
+    context['hoje'] = date.today()
+    return render(request, "index.html", context)
